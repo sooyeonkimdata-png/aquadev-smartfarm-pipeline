a0601847c0ba9ac79f1f452eb79cc49c282163b7
Sooyeon Kim <sooyeonkim.data@gmail.com>
2026-05-07 00:55:39 +0900

    3일치 데이터 기준으로 코드 복구

--- dashboard.py
+++ dashboard.py
@@ -16,6 +16,7 @@ st.set_page_config(
 
 DB_PATH = Path("./output/aquafarm.db")  # db 경로를 변수에 미리 저장
 
+
 st.markdown("""
     <style>
     [data-testid="stMetricLabel"] p {
@@ -49,6 +50,7 @@ THRESHOLDS = {
 
 
 # (Streamlit 대시보드로 효율적으로 불러오기 위한) db 조회 함수
+@st.cache_data(ttl=1800)  # 30분마다 캐시 갱신(Time to Live), 반환되는 결과값은 메모리에 저장 (쿼리로 호출 시 db 대신 메모리에서 조회)
 def load_data(query):     
     con = sqlite3.connect(DB_PATH)  # sqlite3로 db에 연결
     df  = pd.read_sql(query, con)   # 쿼리 결과를 df로 반환
@@ -71,17 +73,14 @@ def load_alerts():
         LIMIT 50
     """)
 
-# 특정 수조 선택 or 조회 기간 변경 시, 동적 필터링 적용 (캐시 없이 실시간 조회)
+# 특정 수조 선택 or 조회 기간 변경 시, 동적 필터링 적용
 def load_timeseries(tank_id, days=7):
-    con = sqlite3.connect(DB_PATH)
-    df = pd.read_sql(f"""
+    return load_data(f"""
         SELECT * FROM sensor_timeseries
         WHERE tank_id = '{tank_id}'
-        ORDER BY timestamp DESC
-        LIMIT {days * 24 * 2}
-    """, con)
-    con.close()
-    return df.sort_values("timestamp")
+          AND timestamp >= datetime('now', '+9 hours', '-{days} days')
+        ORDER BY timestamp
+    """)
     
 # 헤더
 col1, col2 = st.columns([3, 1])  # 화면 비율 나누기 -> 제목, 현재 시간 배치
